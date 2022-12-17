f1eb8016c5d9a7dba67179fb99bfb6e46ad81fee
Damir Muhamedzhanov <rowe.1992@bk.ru>
2022-12-17 23:00:18 +0600

    Example005_ConditionifElse

---
+++ Example005_ConditionifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() =="дина")
+{
+Console.WriteLine("Ура это же ДИНА!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
